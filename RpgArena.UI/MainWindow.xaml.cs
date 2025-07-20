1ac53b08c8116be091a8557e31d1e19a368f6f3d
Zack <venhuizen.zack@gmail.com>
2025-07-19 21:51:35 -0500

    Create Menu Options

--- RpgArena.UI/MainWindow.xaml.cs
+++ RpgArena.UI/MainWindow.xaml.cs
@@ -9,15 +9,45 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RpgArena;
-
+namespace RpgArena.UI
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ContinueButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new GameSelectionWindow(/*pass "continue" flag */);
+            window.Show();
+        }
+        private void NewCampaignButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new CampaignWindow();
+            window.Show();
+        }
+
+        private void CharactersButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new CharactersWindow();
+            window.Show();
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new SettingsWindow();
+            window.Show();
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new HelpWindow();
+            window.Show();
+        }
     }
 }
